e059a24f1d268556dbf28655238052201d92f808
stefanboonstra <stefanboonstra@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-25 09:19:20 +0000

    Fix for servers that have disabled the 'file_get_contents' function.
    git-svn-id: https://plugins.svn.wordpress.org/slideshow-jquery-image-gallery/trunk@563456 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/Slideshow.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Class Slideslow is called whenever a slideshow do_action tag is come across.
+ * Responsible for outputting the slideshow's HTML, CSS and Javascript.
+ *
+ * @author: Stefan Boonstra
+ * @version: 23-06-12
+ */
+class Slideshow {
+
+	/**
+	 * Function deploy prints out the prepared html
+	 *
+	 * @param int $postId
+	 */
+	static function deploy($postId = ''){
+		echo self::prepare($postId);
+	}
+
+	/**
+	 * Function prepare returns the required html and enqueues
+	 * the scripts and stylesheets necessary for displaying the slideshow
+	 *
+	 * Passing this function no parameter or passing it a negative one will
+	 * result in a random pick of slideshow
+	 *
+	 * @param int $postId
+	 * @return String $output
+	 */
+	static function prepare($postId = ''){
+		// Check if defined which Slideshow to use
+		if(empty($postId) || !is_numeric($postId) || $postId < 0){
+			$post = get_posts(array(
+				'numberposts' => 1,
+				'orderby' => 'rand',
+				'post_type' => SlideshowPostType::$postType
+			));
+
+			if(is_array($post))
+				$post = $post[0];
+		}else
+			$post = wp_get_single_post($postId);
+
+		// Exit function on error
+		if(empty($post))
+			return;
+
+		// Get settings
+		$settings = SlideshowPostType::getSettings($post->ID);
+
+		// Load images into array
+		$images = array();
+		$imageObjects = SlideshowPostType::getAttachments($post->ID);
+		foreach($imageObjects as $key => $imageObject){
+			$images[$key] = array(
+				'img' => $imageObject->guid,
+				'title' => $imageObject->post_title,
+				'description' => $imageObject->post_content,
+				'url' => $imageObject->guid
+			);
+		}
+
+		// Check in what way the stylesheet needs to be loaded, .css can be enqueued, custom styles need to be printed.
+		$printStyle = '';
+		if($settings['style'] == 'custom-style') // Enqueue stylesheet
+			$printStyle = $settings['custom-style'];
+		else // Custom style, print it.
+			wp_enqueue_style(
+				'slideshow_style',
+				SlideshowMain::getPluginUrl() . '/style/' . __CLASS__ . '/' . $settings['style']
+			);
+
+		// Include output file that stores output in $output.
+		$output = '';
+		ob_start();
+		include(SlideshowMain::getPluginPath() . '/views/' . __CLASS__ . '/slideshow.php');
+		$output .= ob_get_clean();
+
+		// Enqueue slideshow script
+		wp_enqueue_script(
+			'slideshow_script',
+			SlideshowMain::getPluginUrl() . '/js/' . __CLASS__ . '/slideshow.js',
+			array('jquery'),
+			false,
+			true
+		);
+
+		// Return output
+		return $output;
+	}
+}
---
+++ views/SlideshowPostType/information.php
@@ -0,0 +1,10 @@
+<p>
+	<? echo sprintf(
+		'<p>' . __('To use this slideshow in your website either add this piece of shortcode to your posts or pages', 'slideshow-plugin') . ':</p>
+		<p><i>%s<i></p>
+		<p>' . __('Or add this piece of code to where ever in your website you want to place the slideshow', 'slideshow-plugin') . ':</p>
+		<p><i>%s</i></p>',
+		$shortCode,
+		$snippet
+	); ?>
+</p>
